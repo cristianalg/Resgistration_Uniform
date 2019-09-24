c6b1b9e5c566e30515243e9c49382a6e4c4bcaae
Cristiana <cristianalg7@hotmail.com>
2019-09-24 12:18:47 +0100

    Definido o Layout de SignUP e Login,mas não contém ações. Falta criar a classe de base de dados e de validações de campos.

---
+++ app/src/main/java/ipt/ipg/app_uniform/controllers/MainActivity.java
@@ -0,0 +1,38 @@
+package ipt.ipg.app_uniform.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import ipt.ipg.app_uniform.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int TIME_OUT = 2000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        final View myLayout = findViewById(R.id.launch);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, TIME_OUT);
+    }
+}
